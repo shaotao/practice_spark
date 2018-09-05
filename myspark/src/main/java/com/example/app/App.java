b34cd5a44b0c2c866aa644e461943fa5c759dda5
Shao Tao <shaotao@yahoo.com>
2018-09-05 20:58:15 +0800

    add myspark code base.

---
+++ myspark/src/main/java/com/example/app/App.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+/**
+ * my spark program
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ myspark/src/main/java/com/example/app/config/ConfigUtil.java
@@ -0,0 +1,4 @@
+package com.example.app.config;
+
+public class ConfigUtil {
+}
